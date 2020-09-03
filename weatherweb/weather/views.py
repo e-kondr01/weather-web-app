14db98799a3530da70c7277398f85ccd2c7ad173
e-kondr01 <e.kondr01@gmail.com>
2020-09-03 19:23:18 +0300

    apis changed

--- weatherweb/weather/views.py
+++ weatherweb/weather/views.py
@@ -11,36 +11,33 @@ def index(request):
 
 def main(request):
     """Gets weather forecast for a week from three sources:
-    Gismeteo, weather.com and yandex. Returns
+    OpenWeather, weather.com and yandex. Returns
     JSON results and an average result."""
     #  Location params
     latitude = 60
     longitude = 30
-    days = 3
+    days = 5
 
     #  Weather.com API
     weathercom_json = requests.get(f'http://api.weather.com/v1/geocode/{latitude}/{longitude}/forecast/daily/{days}day.json',
         params={'apiKey': 'dc5ea0e10f11465f9ea0e10f11e65fa6'})
 
-    #  Gismeteo API
-    gismeteo_json = requests.get('https://api.gismeteo.net/v2/weather/forecast/aggregate/4368',
-        params={'latitude': latitude, 'longitude': longitude, 'days': days},
-        headers={'X-Gismeteo-Token': '56b30cb255.3443075'})
-    
-    print(gismeteo_json.request.headers)
+    #  OpenWeather API
+    openweather_json = requests.get('https://api.openweathermap.org/data/2.5/onecall',
+        params={'lat': latitude, 'lon': longitude, 'appid': 'c2788a7691e78525795de7170c9f349e'})
 
-    #  Yandex API
-    yandex_json = requests.get('https://api.weather.yandex.ru/v2/informers',
-        headers={'X-Yandex-API-Key': 'c54630e9-d293-44b1-ab1c-3add95fa374c'},
-        params={'lat': latitude, 'lon': longitude},
+    #  WeatherBit API
+    weatherbit_json = requests.get('https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly',
+        headers={'x-rapidapi-host': "weatherbit-v1-mashape.p.rapidapi.com",
+                 'x-rapidapi-key': "7364d7fb71msh2d462a6fff9d1b1p102b53jsnff2949c01c27"},
+        params={'lat': latitude, 'lon': longitude, "lang":"en"},
         )
 
-    print(yandex_json.request.headers)
     #  Processing data
     pass
 
     return render(request, 'weatherweb/weather.html', {
         'weathercom': weathercom_json.json(),
-        'gismeteo': gismeteo_json.json(),
-        'yandex': yandex_json.json()
+        'openweather': openweather_json.json(),
+        'weatherbit': weatherbit_json.json()
     })
