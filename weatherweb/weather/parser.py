2c99bc132e6384a8b34a934073e7c11e3fff69e9
e-kondr01 <e.kondr01@gmail.com>
2020-09-04 12:56:00 +0300

    gismeteo parsed

--- weatherweb/weather/parser.py
+++ weatherweb/weather/parser.py
@@ -1,18 +1,22 @@
 import json
+import requests
+
+from bs4 import BeautifulSoup
 
 
-def parser(weathercom_json=None, openweather_json=None, weatherbit_json=None):
-    weathercom = parse_weathercom(weathercom_json)
-    openweather = parse_openweather(openweather_json)
-    weatherbit = parse_weatherbit(weatherbit_json)
+def parse_weather():
+    weathercom = parse_weathercom()
+    gismeteo = parse_gismeteo()
+    yandex = parse_yandex()
     parsed_data = {
         'weathercom': weathercom,
-        'openweather': openweather,
-        'weatherbit': weatherbit}
+        'gismeteo': gismeteo,
+        'yandex': yandex}
     return parsed_data
 
 
-def parse_weathercom(weathercom):
+def parse_weathercom():
+    '''
     forecast = weathercom['forecasts']
     parsed_data = []
     for date in forecast:
@@ -26,11 +30,61 @@ def parse_weathercom(weathercom):
         }
         )
     return parsed_data
-
-
-def parse_openweather(openweather):
+    '''
     return None
 
 
-def parse_weatherbit(weatherbit):
+def parse_gismeteo():
+    headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36'
+        }
+    resp = requests.get('https://www.gismeteo.ru/weather-sankt-peterburg-4079/3-days/',
+                        headers=headers)
+    parser = BeautifulSoup(resp.text,
+                           'html.parser')
+
+    temp_spans = parser.find_all(class_='unit unit_temperature_c')
+    temps = []
+    for span in temp_spans:
+        temps.append(span.string)
+
+    '''
+    time_of_day_spans = parser.find_all(class_='time_of_day')
+    time_of_day = []
+    for time in time_of_day_spans:
+        time_of_day.append(time.string)
+        if len(time_of_day) == 12:
+            break
+    '''
+
+    day0 = parser.find('div', attrs={'data-index': '0'})
+    day0 = day0.a.text
+
+    day1 = parser.find('div', attrs={'data-index': '1'})
+    day1 = day1.a.text
+
+    day2 = parser.find('div', attrs={'data-index': '2'})
+    day2 = day2.a.text
+
+    gismeteo = {}
+    days = []
+    for i in range(3):
+        day = {}
+        day['Ночь'] = temps[0 + 4 * i]
+        day['Утро'] = temps[1 + 4 * i]
+        day['День'] = temps[2 + 4 * i]
+        day['Вечер'] = temps[3 + 4 * i]
+        days.append(day)
+
+    gismeteo[day0] = days[0]
+    gismeteo[day1] = days[1]
+    gismeteo[day2] = days[2]
+
+    return gismeteo
+
+
+def parse_yandex():
     return None
+
+
+print(parse_gismeteo())
--- weatherweb/weather/views.py
+++ weatherweb/weather/views.py
@@ -4,7 +4,7 @@ import requests
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from weather.average import average
-from weather.parser import parser
+from weather.parser import parse_weather
 
 
 def index(request):
@@ -13,8 +13,10 @@ def index(request):
 
 def main(request):
     """Gets weather forecast for a week from three sources:
-    OpenWeather, weather.com and yandex. Returns
-    JSON results and an average result."""
+    Gismeteo, weather.com and yandex weather. Returns
+    parsed forecast data and an average result."""
+
+    """
     #  Location params
     latitude = 60
     longitude = 30
@@ -24,6 +26,7 @@ def main(request):
     weathercom_json = requests.get(f'http://api.weather.com/v1/geocode/{latitude}/{longitude}/forecast/daily/{days}day.json',
         params={'apiKey': 'dc5ea0e10f11465f9ea0e10f11e65fa6'}).json()
 
+
     #  OpenWeather API
     openweather_json = requests.get('https://api.openweathermap.org/data/2.5/onecall',
         params={'lat': latitude, 'lon': longitude, 'appid': 'c2788a7691e78525795de7170c9f349e'}).json()
@@ -32,16 +35,14 @@ def main(request):
     weatherbit_json = requests.get('https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly',
         headers={'x-rapidapi-host': "weatherbit-v1-mashape.p.rapidapi.com",
                  'x-rapidapi-key': "7364d7fb71msh2d462a6fff9d1b1p102b53jsnff2949c01c27"},
-        params={'lat': latitude, 'lon': longitude, "lang":"en"}).json()
-
-    #  Processing data
-    parsed_data = parser(weathercom_json=weathercom_json, openweather_json=openweather_json, weatherbit_json=weatherbit_json)
+        params={'lat': latitude, 'lon': longitude, "lang":"en"}).json() 
+    """
+    parsed_data = parse_weather()
     average_json = average(parsed_data)
 
     return render(request, 'weatherweb/weather.html', {
-        'weathercom': weathercom_json,
-        'openweather': openweather_json,
-        'weatherbit': weatherbit_json,
-        'parsed': parsed_data,
+        'weathercom': parsed_data['weathercom'],
+        'gismeteo': parsed_data['gismeteo'],
+        'yandex': parsed_data['yandex'],
         'average': average_json
     })
